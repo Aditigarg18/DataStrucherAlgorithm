d6b3481214440ac4bb6431ec9c3515a4a08ed0c9
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2025-05-05 11:36:51 +0530

    Create pascal's triangle.java

---
+++ striver(A-Z)/Arrays/pascal's triangle.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int ncr(int n,int r){
+    long res=1;
+    for(int i=0;i<r;i++){
+        res=res*(n-i);
+        res=res/(i+1);
+    }
+    return (int)res;
+}
+  public List<List<Integer>> generate(int numRows) {
+        // code here
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int row = 1; row <= numRows; row++) {
+            List<Integer> templist = new ArrayList<>();
+        for(int col=1;col<=row;col++){
+            templist.add(ncr(row-1,col-1));
+        }
+        ans.add(templist);
+    }
+    return ans;
+  }
+}
