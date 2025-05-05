0c56e0b10a08ce29316fde1961c785c2f9a2cb91
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2025-05-05 12:01:10 +0530

    Update and rename Spirally traversing a matrix.java to 1.Spirally traversing a matrix.java

--- gfg160/Matrix/1.Spirally traversing a matrix.java
+++ gfg160/Matrix/1.Spirally traversing a matrix.java
@@ -1,3 +1,13 @@
+//leetcode 54
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [1,2,3,6,9,8,7,4,5]
 class Solution {
     // Function to return a list of integers denoting spiral traversal of matrix.
     public ArrayList<Integer> spirallyTraverse(int mat[][]) {
