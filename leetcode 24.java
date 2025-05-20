ffdd15dd30b1403b7a909d8a23edb947d94b661e
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2025-05-20 12:52:33 +0530

    Update leetcode 24.java

--- leetcode 24.java
+++ leetcode 24.java
@@ -1,11 +1,29 @@
 //Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if(head==null || head.next==null)return head;
-        // A=HEAD,B=HEAD.NEXT,C=HEAD.NEXT.NEXT
-       ListNode temp=head.next;
-        head.next=swapPairs(head.next.next);
-    temp.next=head;
-        return temp;
+   public ListNode swapPairs(ListNode head) {
+        // Dummy node acts as the prevNode for the head node
+        // of the list and hence stores pointer to the head node.
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+
+        while (head != null && head.next != null) {
+            // Nodes to be swapped
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            // Swapping
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            // Reinitializing the head and prevNode for next swap
+            prevNode = firstNode;
+            head = firstNode.next; // jump
+        }
+
+        // Return the new head node.
+        return dummy.next;
     }
 }
